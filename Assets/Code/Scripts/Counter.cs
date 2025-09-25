c62695e12628535ff9a2df870f0f576dfe0c3f1f
Sofia Buryachenko <buryachenko.sofia@gmail.com>
2025-09-25 20:34:14 +0300

    feat: add Counter

---
+++ Assets/Code/Scripts/Counter.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    private int moves = 0, record = 0;
+    [SerializeField] private Text text;
+    
+    private void Awake()
+    {
+        if (!PlayerPrefs.HasKey("record"))
+        {
+            record = 0;
+            PlayerPrefs.SetInt("record", record);
+        }
+        else
+        {
+            record = PlayerPrefs.GetInt("record");
+        }
+    }
+
+    public void Add()
+    {
+        moves++;
+    }
+
+    private void UpdateText()
+    {
+        text.text = moves + "\n";
+        if (record != 0)
+        {
+            text.text += "Record: " + record;
+        }
+    }
+}
