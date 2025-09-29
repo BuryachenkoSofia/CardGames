780403f0e0aef3a4a870821febc1ff1865fd65d3
Sofia Buryachenko <buryachenko.sofia@gmail.com>
2025-09-29 18:18:41 +0300

    chore: DeckManager

--- Assets/Code/Scripts/DeckManager.cs
+++ Assets/Code/Scripts/DeckManager.cs
@@ -8,7 +8,7 @@ public class DeckManager : MonoBehaviour
     private void Awake()
     {
         CreateDeck();
-        ShuffleDeck();
+        ShuffleDeckSolitaire();
     }
 
     private void CreateDeck()
@@ -31,14 +31,54 @@ public class DeckManager : MonoBehaviour
     private void ShuffleDeck()
     {
         System.Random rng = new System.Random();
-        int n = deck.Count;
+        ShuffleList(deck, rng);
+    }
+
+    private void ShuffleDeckSolitaire()
+    {
+        List<CardData> playerCards = new List<CardData>(); 
+        List<CardData> restCards = new List<CardData>(deck);
+        System.Random rng = new System.Random();
+        foreach (Rank rank in System.Enum.GetValues(typeof(Rank)))
+        {
+            List<CardData> blacks = restCards.FindAll(c => c.rank == rank && (c.suit == Suit.Spades || c.suit == Suit.Clubs));
+            if (blacks.Count > 0)
+            {
+                CardData chosen = blacks[rng.Next(blacks.Count)];
+                playerCards.Add(chosen);
+                restCards.Remove(chosen);
+            }
+            List<CardData> reds = restCards.FindAll(c => c.rank == rank && (c.suit == Suit.Hearts || c.suit == Suit.Diamonds));
+            if (reds.Count > 0)
+            {
+                CardData chosen = reds[rng.Next(reds.Count)];
+                playerCards.Add(chosen);
+                restCards.Remove(chosen);
+            }
+        }
+        while (playerCards.Count < 24 && restCards.Count > 0)
+        {
+            int idx = rng.Next(restCards.Count);
+            playerCards.Add(restCards[idx]);
+            restCards.RemoveAt(idx);
+        }
+        ShuffleList(playerCards, rng);
+        ShuffleList(restCards, rng);
+        deck.Clear();
+        deck.AddRange(restCards);
+        deck.AddRange(playerCards);
+    }
+
+    private void ShuffleList(List<CardData> list, System.Random rng)
+    {
+        int n = list.Count;
         while (n > 1)
         {
             n--;
             int k = rng.Next(n + 1);
-            CardData temp = deck[k];
-            deck[k] = deck[n];
-            deck[n] = temp;
+            CardData temp = list[k];
+            list[k] = list[n];
+            list[n] = temp;
         }
     }
 }
